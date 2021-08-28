96d0381509b7de1b52cca293d5b12cf68f7cfbe7
Dzmitry Dutsin <dzmitry.dutsin@refinitiv.com>
2021-08-28 16:55:43 +0300

    initial commit

--- projects/core/src/lib/divider/divider.component.ts
+++ projects/core/src/lib/divider/divider.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'acc-divider',
--- projects/core/src/lib/icon/icon.component.ts
+++ projects/core/src/lib/icon/icon.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit, Input } from '@angular/core';
 import { Icon } from './icon.interface';
 
 @Component({
